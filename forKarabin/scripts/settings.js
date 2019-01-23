8b93422e8d2ad87d9a089ad18ffd29d8997f3df1
uranokian <uranokian73@gmail.com>
2019-01-23 18:34:26 +0200

    komKarabin4

--- forKarabin/scripts/settings.js
+++ forKarabin/scripts/settings.js
@@ -1,12 +1,4 @@
 (function() {
-    let heightOfNavMenu = document.querySelector(".navigation-wrapper").clientHeight;
-    console.log(heightOfNavMenu);
-    let heightofTopHeader = document.querySelector(".top-header").clientHeight;
-    console.log(heightofTopHeader);
-    heightofTopHeader = heightofTopHeader - heightOfNavMenu;
-    console.log(heightofTopHeader);
-    document.querySelector(".top-header").style.height = heightofTopHeader + "px"; 
-
     window.onscroll = function() {
         let scrolled = window.pageYOffset;
         let navMenu = document.querySelector(".navigation-wrapper");
