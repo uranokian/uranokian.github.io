60faa211889ad38f51c856933bb2f62d14e2558f
uranokian <uranokian73@gmail.com>
2019-01-23 18:55:55 +0200

    forKarabin

--- forKarabin/scripts/settings.js
+++ forKarabin/scripts/settings.js
@@ -1,14 +1,4 @@
 (function() {
-    window.onscroll = function() {
-        let scrolled = window.pageYOffset;
-        let navMenu = document.querySelector(".navigation-wrapper");
-        if(scrolled > 0) {
-            navMenu.classList.add("navigation-wrapper-scrolled");
-        } else {
-            navMenu.classList.remove("navigation-wrapper-scrolled");
-            navMenu.classList.add("navigation-wrapper");
-            
-            }
-      }
+    
     
 })();
