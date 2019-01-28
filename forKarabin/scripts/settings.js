35451cd1944519a8fb723bb3f526c90bec267c42
uranokian <uranokian73@gmail.com>
2019-01-28 17:59:27 +0200

    karabin2

--- forKarabin/scripts/settings.js
+++ forKarabin/scripts/settings.js
@@ -1,4 +1,34 @@
 (function() {
-    
-    
+    const FirstColor = "transparent";
+    const SecondColor = "#2d6b22";
+    const buyButton = $(".card-wrapper__buy-button");
+    $('.card-wrapper').hover(
+        function() {
+            $(this).find(".card-wrapper__buy-button").css({
+                "background-color": "#072a16",
+                "color": "white",
+            });
+        },
+        function() {
+            $(this).find(".card-wrapper__buy-button").css({
+                "background-color": FirstColor,
+                "color": "rgba(0, 0, 0, 0.774)",   
+            });
+        });
+    buyButton.hover(
+        function() {
+            $(this).css("background-color", SecondColor);
+        },
+        function() {
+            $(this).css({
+                "background-color": "#072a16",
+                "color": "white"
+            });
+        }
+    );
+
+    buyButton.click(function() {
+        location.href = 'https://web.telegram.org/#/im?p=@romadench';
+    });
+
 })();
