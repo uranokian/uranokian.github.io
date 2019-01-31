4c8cfc907a8cd0285e22d137cbc94fa6e1eb6ecc
uranokian <uranokian73@gmail.com>
2019-01-31 18:09:55 +0200

    karabinNew-2

--- testPage/scripts/top-menu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(document).ready(function(){
-        $('.menu-button').click(function(){
-            $('.navigation-menu').toggleClass('navigation-menu-hidden');
-        });
-    });
-});
