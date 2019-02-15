3021e1d73078c59437620ead28b4f2a2c7edf49b
uranokian <uranokian73@gmail.com>
2019-02-15 10:53:04 +0200

    home-staging 1.1.2

--- home-staging/scripts/header-slider.js
+++ home-staging/scripts/header-slider.js
@@ -1,6 +1,21 @@
 const SLIDER = function(){
 
      const urls = ["photos/header-background.jpg", "photos/header-background-2.jpg"];
-
+     let photoWidth = $(".slidewrapper__slide-img").width();
+     let amountOfPhoto = $(".slidewrapper__slide-img").length;
+     console.log(amountOfPhoto);
+     
+     
+     count = 1;
+     setInterval(function() {
+          if (count == amountOfPhoto) {
+               return;
+          }
+          $(".slidewrapper").css({
+               'left': `${-photoWidth*count}px`,
+          });
+          count += 1;
+          
+     }, 10000);
                
 }();
